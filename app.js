b3e1ea0bae785f9907a19c45bd85abf099035c75
roushan-sheik <roushansheik@gmail.com>
2024-02-17 07:28:52 +0600

    access the name

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const allButtons = document.getElementsByClassName("add-btn");
 
 for (const btn of allButtons) {
   btn.addEventListener("click", (event) => {
-    console.log(event.target.parentNode.childNodes[1]);
+    const name = event.target.parentNode.childNodes[1].innerText;
+    console.log();
   });
 }
