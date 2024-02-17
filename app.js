009fdf393b11a712c9077e75ee4c961c95512aff
roushan-sheik <roushansheik@gmail.com>
2024-02-17 07:27:59 +0600

    go to parent and access the child

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const allButtons = document.getElementsByClassName("add-btn");
 
 for (const btn of allButtons) {
   btn.addEventListener("click", (event) => {
-    console.log(event.target);
+    console.log(event.target.parentNode.childNodes[1]);
   });
 }
