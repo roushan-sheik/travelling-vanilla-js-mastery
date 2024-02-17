0d8310901701d0878c4911455c0bb164d69595e8
roushan-sheik <roushansheik@gmail.com>
2024-02-17 07:37:21 +0600

    set the cart counter value and access the price

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const allButtons = document.getElementsByClassName("add-btn");
-
+let count = 0;
 for (const btn of allButtons) {
   btn.addEventListener("click", (event) => {
+    // update the counter
+    count += 1;
+    // set the counter value
+    document.getElementById("cart-count").innerHTML = count;
+    // access name and price
     const name = event.target.parentNode.childNodes[1].innerText;
-    console.log();
+    const price = event.target.parentNode.childNodes[3].innerText;
+    console.log(name, price);
   });
 }
