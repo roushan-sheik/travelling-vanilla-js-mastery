8b735c4100571059682ed6afea48769029fff9a6
roushan-sheik <roushansheik@gmail.com>
2024-02-17 07:26:12 +0600

    get target element by click

---
+++ app.js
@@ -0,0 +1,7 @@
+const allButtons = document.getElementsByClassName("add-btn");
+
+for (const btn of allButtons) {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
+}
